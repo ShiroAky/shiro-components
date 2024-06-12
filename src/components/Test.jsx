d5a69544df2e62f6849c7a0ef84bdc3b21692308
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2024-06-11 20:01:57 -0400

    starting project

---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+import { FaStamp } from 'react-icons/fa'
+
+export default function Test() {
+  return (
+    <div className='w-full h-full flex flex-col items-center justify-center bg-slate-900'>
+      <h1 className='text-2xl text-slate-300'>Hello world!! <FaStamp /></h1>
+    </div>
+  )
+}
